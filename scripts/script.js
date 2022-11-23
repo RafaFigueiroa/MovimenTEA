545c77a16e49f564dcec08290ca8b2d23756196f
Rafael <rafaelmsfigueiroa@gmail.com>
2022-11-23 17:28:22 -0300

    alterações

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function abrir(){
+    document.getElementsByClassName('popup-comunicacao').style.display = 'block';
+}
+
+function fechar(){
+    document.getElementById('popup-comunicacao').style.display = 'none';
+}
